f599eccb880ed697d60f46a060c0542af0a2d562
shamir0xe <shamir0xe@gmail.com>
2024-04-30 11:58:42 +0330

    feat: add BotSession/RequestBotSessionResolver

--- src/api/mutations.py
+++ src/api/mutations.py
@@ -1,13 +1,17 @@
 from typing import Optional
 import strawberry
+from src.api.types.bot_session import BotSession
+from src.resolvers.bot.request_bot_session_resolver import RequestBotSessionResolver
 from src.api.types.response import Response
 from src.api.types.user import User
 from src.api.types.bot import Bot
+
 # from src.api.types.server import Server
 
 from src.resolvers.bot.create_bot_resolver import CreateBotResolver
 from src.resolvers.database.run_seeders_resolver import RunSeedersResolver
 from src.resolvers.user.create_user_resolver import CreateUserResolver
+
 # from src.resolvers.server.create_server_resolver import CreateServerResolver
 from src.resolvers.auth.login_resolver import LoginResolver
 from src.resolvers.auth.logout_resolver import LogoutResolver
@@ -28,6 +32,14 @@ class Mutation:
 
     # @auth
     update_bot: Bot = strawberry.field(**UpdateBotResolver.pack())
+
+    # @role(admin)
+    request_bot_session: BotSession = strawberry.field(
+        **RequestBotSessionResolver.pack()
+    )
+
+    # @role(admin)
+    # update_bot_session: BotSession = strawberry.field(**UpdateBotSessionResolver.pack())
 
     # @auth @admin create_server(host, port)
     # create_server: Server = strawberry.field(**CreateServerResolver.pack())
---
+++ src/resolvers/bot/request_bot_session_resolver.py
@@ -0,0 +1,35 @@
+import strawberry
+from src.actions.user.create_user import CreateUser
+from src.finders.bot_finder import BotFinder
+from src.finders.user_finder import UserFinder
+from src.helpers.types.user_roles import UserRoles
+from src.models.bot.bot_session import BotSession
+from src.resolvers.base_resolver import BaseResolver
+from src.authorizations.authorization import role
+from src.types.exception_types import ExceptionTypes
+
+
+class RequestBotSessionResolver(BaseResolver):
+    desc = "Request new bot session"
+
+    @role(UserRoles.ADMIN)
+    @staticmethod
+    def fn(
+        bot_id: str = strawberry.UNSET,
+        telegram_id: str = strawberry.UNSET,
+        info: strawberry.Info = strawberry.UNSET,
+    ) -> str:
+        bot = BotFinder.by_id(bot_id)
+        if not bot:
+            raise Exception(ExceptionTypes.NO_SUCH_OBJECT_EXISTS)
+        user = UserFinder.by_telegram_id(telegram_id)
+        if not user:
+            user = CreateUser.with_telegram_id(telegram_id)
+            if not user:
+                raise Exception(ExceptionTypes.INTERNAL_ERROR)
+        data = {"user_id": user.pk, "bot_id": bot.id, "data": ""}
+        bot_session = BotSession(**data)
+        bot_session.save()
+        if not bot_session.pk:
+            raise Exception(ExceptionTypes.INTERNAL_ERROR)
+        return bot_session.pk
